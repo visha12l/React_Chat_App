60a200fc1872b305620d695f617d3d3dec595376
visha12l <vsvshinde15@gmail.com>
2018-08-16 19:31:10 +0530

    added react scrollbar

--- src/javascript/components/message_list.js
+++ src/javascript/components/message_list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import underscore from 'underscore';
+import ScrollArea from 'react-scrollbar';
 
 export default class MessageList extends React.Component {
     constructor() {
@@ -9,12 +10,16 @@ export default class MessageList extends React.Component {
     render() {
         let { message } = this.props;
         return (
-            <ul className='message-list'>
-                {message && message.length
-                   ? underscore.map(message, (user, key) => <li key={key} className='message'><p>{user.senderId}</p><p>{user.text}</p></li>)
-                   : null
-                 }
-            </ul>
+            <ScrollArea
+              speed={0.8}
+              horizontal={false} >
+                <ul className='message-list'>
+                    {message && message.length
+                       ? underscore.map(message, (user, key) => <li key={key} className='message'><p>{user.senderId}</p><p>{user.text}</p></li>)
+                       : null
+                     }
+                </ul>
+            </ScrollArea>
         );
     }
 }
